862f62533e1c3b3d520ea52f368a14ff835bf5e6
ShobanaMuthiah <shobanam222@gmail.com>
2026-04-15 10:08:18 +0530

    bug fixed

--- BackEnd/src/Controllers/chatController.ts
+++ BackEnd/src/Controllers/chatController.ts
@@ -46,7 +46,7 @@ export const userChatList = async (req: Request, res: Response, next: NextFuncti
         .distinctOn(["sendId", "receiverId"])
 
         .where("chats.userId=:userId  OR chats.receiverId=:userId", { userId })
-        .groupBy("sendId, receiverId, sender.username, receiver.username")
+        .groupBy("sendId, receiverId, sender.username, receiver.username,chats.userId")
         .getRawMany();
     res.status(200).json({ chats }).end();
 }
